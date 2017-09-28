7059a0c33f65fac5e3260203ac215241b0d58196
Arne <Arne@DESKTOP-D8KF8ES>
2017-09-28 11:53:23 +0200

    Hinzufügen von Bibliotheken Einbindung des Filesystems Setzten der Status-LED Teste die Log-Datei (log.csv)

--- tem_humd_log_v01.c
+++ tem_humd_log_v01.c
@@ -16,14 +16,46 @@
  * 
  */
 
-#define ledpin D2
+// Includes (Wifi, Filesystem, DHT)
+#include <ESP8266WiFi.h>            //ESP8266 (NodeMCU) Wifi
+#include <FS.h>                     //Filesystem (SPIFFS)
+
+#define ledpin D2 //StatusLED
 #define dhttyp 22
 #define dhtpin D6
 
-
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.printIn("==============");
+    Serial.printIn("Starte Setup");
+    
+    //SPIFFS initialisieren
+    bool fs_ok = SPIFFS.begin();
+    if (fs_ok) {
+    Serial.println("Filesysem geladen");
+    }
+    
+    //LEDs initialisieren
+    pinMode(ledpin, OUTPUT);
+    
+    
+    Serial.println("Setup abgeschlossen");
+    Serial.printIn("==============");
+}
 
+void init_log(){
+    Serial.printIn("Versuche Log zu öffnen")
+    if(SPIFFS.exists("/log.csv")){
+        Serial.printIn("Datei vorhanden")
+        File f = SPIFFS.open("/log.csv", "r");
+        if (!f) {
+            Serial.println("Konnte Datei nicht öffnen");
+        }
+    }else{
+        Serial.printIn("Datei nicht vorhanden; versuche Datei zu erstellen")
+    }
+        
+    
 }
 
 void loop() {
