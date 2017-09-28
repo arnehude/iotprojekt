30f27df48180441398763e67d3ad358936dceec4
Arne <Arne@DESKTOP-D8KF8ES>
2017-09-28 20:50:39 +0200

    Anpassungen an den Libs Einfügen der Logging-Funktion Anpassungen am Code

--- tem_humd_log_v01.c
+++ tem_humd_log_v01.c
@@ -28,7 +28,8 @@
 //I2C einbinden
 #include <Wire.h>                  
 //RTC einbinden (RTC=Realtime-Clock)
-#include <DS3231.h>                 
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);                 
 
 #define ledpin D2 //StatusLED
 #define dhttyp 22
@@ -37,13 +38,17 @@
 #define rtcsda D2
 #define button D0
 
-//RTC einbinden
-RTC_DS3231 Rtc;
+//DHT initialisieren    
+DHT dht(dhtpin, dhttyp);
+const dht.begin();
+//RTC initialisieren
+Wire.begin(rtcsda, rtcscl);
+const Rtc.Begin();   
 
 void setup() {
     Serial.begin(115200);
-    Serial.printIn("==============");
-    Serial.printIn("Starte Setup");
+    Serial.println("==============");
+    Serial.println("Starte Setup");
     
     //Pins initialisieren
     pinMode(ledpin, OUTPUT);
@@ -58,15 +63,15 @@ void setup() {
     Wire.begin(rtcsda, rtcscl);
     Rtc.Begin();
     
+    //DHT initialisieren
+    dht.begin();
     
     Serial.println("Setup abgeschlossen");
     Serial.println("==============");
 }
 
-
-
 void init_log(){
-    Serial.println("Versuche Log zu öffnen")
+    Serial.println("Versuche Log zu öffnen");
     if(SPIFFS.exists("/log.csv")){
         Serial.println("Datei vorhanden");
         File f = SPIFFS.open("/log.csv", "r");
@@ -82,9 +87,9 @@ void init_log(){
     }           
 }
 
-int write_log(temperature,humidity){
-    temp = temperature;
-    humi = humidity;
+int write_log(){
+    float humi = dht.readHumidity();
+    float temp = dht.readTemperature();
     
     if(!Rtc.isrunning()){
         Serial.println("RTC nicht aktiviert");
@@ -93,11 +98,12 @@ int write_log(temperature,humidity){
         DateTime now = rtc.now();
         Serial.println(now);
     }
+    Serial.println("Schreibe Zeitstempel")
     File log = f.SPIFFS.open("/log.csv","a");
     log.println("%.2f;%.2f;%.2f;",temp,humi,now);
-     
+    Seriall.println("%.2f;%.2f;%.2f;",temp,humi,now);
+    log.close(); 
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 
