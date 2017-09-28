f093227aa9968691f16d58ace665541feabb126d
Arne <Arne@DESKTOP-D8KF8ES>
2017-09-28 11:07:01 +0200

    Init Temperatur und Humiditäsprogrammes Definition der Pin's

---
+++ tem_humd_log_v01.c
@@ -0,0 +1,31 @@
+/* 
+ * File:   tem_humd_log_v01.c
+ * Author: Arne Hude, Phil Elfers
+ *
+ * Created on 28. September 2017, 11:05
+ */
+/*##################################
+ * IoT-Projekt
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#define ledpin D2
+#define dhttyp 22
+#define 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
